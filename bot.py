5ab80439ea2bbf0a0f8a8bf485ef78d6462908e8
PM-KIRILL <130344954+PM-KIRILL@users.noreply.github.com>
2023-07-15 09:31:19 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,123 @@
+import discord
+import asyncio
+import threading
+import os
+import struct
+import time
+
+import chatlink
+import config
+from discord.ext import commands, tasks
+from utils import rcon, query, ping
+
+class BotClient(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        self.chatlink_object = chatlink.Chatlink()
+        super().__init__(*args, **kwargs)
+        self.thread = threading.Thread(target=self.thread_function, daemon=True)
+        self.loop = asyncio.get_event_loop()
+
+        query_address_str = config.query_address.split(":")[0]
+        query_port = int(config.query_address.split(":")[1])
+        self.query_address = (query_address_str, query_port)
+        self.query = query.Query(self.query_address)
+
+        rcon_split = config.rcon_address.split(":")
+        rcon_address = rcon_split[0]
+        rcon_port = int(rcon_split[1])
+        self.rcon = rcon.RCON(rcon_address, rcon_port, config.rcon_password)
+
+        server_address_split = config.server_address.split(":")
+        server_port = int(server_address_split[1])
+        self.server_address = (server_address_split[0], server_port)
+
+        if config.custom_status == True:
+            print("Custom Discord status is enabled.")
+            self.status_thread_function.start()
+
+    @tasks.loop(seconds=30)
+    async def status_thread_function(self):
+        if config.use_query == True:
+            stats = self.query.full_stat()
+            stats["protocol_version"] = "Unknown"
+        else:
+            pinger = ping.Pinger(self.server_address)
+            stats = pinger.ping_converted()
+        new_status_message = config.custom_status_message.format(**stats)
+        if not new_status_message == self.status_message:
+            activities = {
+                "playing": discord.Game(new_status_message),
+                "streaming": discord.Activity(type=discord.ActivityType.streaming, name=new_status_message),
+                "listening": discord.Activity(type=discord.ActivityType.listening, name=new_status_message),
+                "watching": discord.Activity(type=discord.ActivityType.watching, name=new_status_message),
+                "competing": discord.Activity(type=discord.ActivityType.competing, name=new_status_message)
+            }
+            activity = activities[config.custom_status_type]
+            await self.change_presence(activity=activity)
+        self.status_message = new_status_message
+
+    @status_thread_function.before_loop
+    async def wait_for_ready(self):
+        await self.wait_until_ready()
+        self.status_message = ""
+
+    def thread_function(self):
+        channel = self.get_channel(config.bot_channel_id)
+        print("Chat linked to channel #{channel} ({channelid})".format(channel=channel.name,
+                                                                 channelid=channel.id))
+        for text in self.chatlink_object.main():
+            print("MC -> Discord: "+text)
+            asyncio.run_coroutine_threadsafe(channel.send(text), self.loop)
+
+    async def on_ready(self):
+        print(f'Bot connected as {self.user}')
+        self.thread.start()
+
+        if config.console_channel == True:
+            console_channel = self.get_channel(config.console_channel_id)
+            print("Console linked to channel #{channel} ({channelid})"
+                  .format(channel=console_channel.name, channelid=console_channel.id))
+
+    async def on_message(self, message):
+        if message.author.id == self.user.id:
+            return
+        elif message.author.bot and config.discord_ignore_bots:
+            return
+        await self.process_commands(message)
+
+        if message.content.startswith(config.command_prefix):
+            return
+        if message.channel.id == config.bot_channel_id:
+            nickname = message.author.display_name
+            placeholders = {
+                "user": message.author.display_name,
+                "message": message.content,
+                "reply_user": None,
+                "reply_message": None
+            }
+            
+            if not message.reference == None:
+                reply = message.reference.resolved
+                reply_member = await message.guild.fetch_member(reply.author.id)
+                placeholders["reply_user"] = reply_member.display_name
+                placeholders["reply_message"] = reply.content
+                message_formatted = config.discord_reply_message.format(**placeholders).replace("'", r"\'").replace('"', r'\"')
+            else:
+                message_formatted = config.discord_to_mc_message.format(**placeholders).replace("'", r"\'").replace('"', r'\"')
+                
+            command = 'tellraw @a "{msg}"'.format(msg=message_formatted)
+            try:
+                self.rcon.send_cmd(command)
+            except (BrokenPipeError, ConnectionRefusedError, struct.error) as e:
+                print("Attempting to reconnect to the RCON server...")
+                try:
+                    self.rcon.reconnect()
+                    self.rcon.send_cmd(command)
+                    print("Reconnection successful!")
+                except ConnectionRefusedError:
+                    print("Connection has been refused. Either the server is still starting or RCON is not enabled.")
+            print("Discord -> MC: " + message_formatted)
+
+bot = BotClient(command_prefix=config.command_prefix)
+bot.init()
+bot.run(config.bot_token)
---
+++ commands.py
@@ -0,0 +1,210 @@
+from utils import query, rcon, motd_renderer, ping
+import discord
+from discord.ext import commands
+import config
+import textwrap
+import struct
+import socket
+from io import BytesIO
+
+def embed_builder(embed_dict, placeholders):
+    for item in embed_dict:
+        if type(embed_dict[item]) is str:
+            embed_dict[item] = embed_dict[item].format(**placeholders)
+    embed = discord.Embed(**embed_dict)
+    if "fields" in embed_dict:
+        for field in embed_dict["fields"]:
+            field["value"] = field["value"].format(**placeholders)
+            embed.add_field(**field)
+    if not embed_dict["footer"] == None:
+        embed.set_footer(text=embed_dict["footer"])
+    return embed
+        
+class Commands(commands.Cog, name="Модуль команд"):
+    def __init__(self, client):
+        self.client = client
+
+    def get_stats(self):
+        if config.use_query == True:
+            stats = self.client.query.full_stat()
+            players = stats.pop("players")
+            stats["protocol_version"] = "Неизвестно"
+        else:
+            stats = self.client.pinger.ping_converted()
+            players = stats.pop("players")
+            
+        if len(players) == 0 or players[0] == "":
+            players = []
+        return stats, players
+
+    #команда для вывода списка игроков
+    @commands.command(name="players")
+    async def players(self, ctx):
+        try:
+            stats, players = self.get_stats()
+        except socket.timeout:
+            await ctx.send(config.timeout_message)
+            return
+        except ConnectionRefusedError:
+            await ctx.send(config.connection_refused_message)
+            return
+        
+        players_list = []
+        for player in players:
+            players_list.append(config.player_list_item.format(player=player))
+        if len(players) > 0:
+            stats["items"] = items="\n".join(players_list)
+        else:
+            stats["items"] = chr(173)
+            
+        message = {}
+        if config.player_list_use_embed:
+            embed = embed_builder(config.player_list_embed, stats)
+            message["embed"] = embed
+        else:
+            response = config.player_list_message.format(**stats)
+            message["content"] = response
+
+        if config.thumbnail_in_player_list and config.player_list_use_embed:
+            message["files"] = []
+            renderer = motd_renderer.MOTDRenderer()
+            ip, port = config.server_address.split(":")
+            thumbnail = renderer.get_thumbnail(address=(ip, int(port)))
+            with BytesIO() as thumbnail_output:
+                thumbnail.save(thumbnail_output, format="PNG")
+                thumbnail_output.seek(0)
+                file = discord.File(fp=thumbnail_output, filename="thumbnail.png")
+                if config.player_list_use_embed:
+                    message["embed"].set_thumbnail(url="attachment://thumbnail.png")
+                message["files"].append(file)
+            
+        await ctx.send(**message)
+
+    #команда для получения различной статистики сервера
+    @commands.command(name="stats")
+    async def stats(self, ctx):
+        try:
+            stats, players = self.get_stats()
+        except socket.timeout:
+            await ctx.send(config.timeout_message)
+            return
+        except ConnectionRefusedError:
+            await ctx.send(config.connection_refused_message)
+            return
+        message = {}
+        if config.stats_use_embed:
+            embed = embed_builder(config.stats_output_embed, stats)
+            message["embed"] = embed
+        else:
+            response = config.stats_output_query.format(**stats).lstrip().strip()
+            message["content"] = response
+
+        renderer = motd_renderer.MOTDRenderer()
+        ip, port = config.server_address.split(":")
+        message["files"] = []
+        if config.motd_image_in_stats:
+            image = renderer.get_full_image(title=config.motd_title, address=(ip, int(port)))
+            with BytesIO() as output:
+                image.save(output, format="PNG")
+                output.seek(0)
+                file = discord.File(fp=output, filename="motd.png")
+                if config.stats_use_embed:
+                    message["embed"].set_image(url="attachment://motd.png")
+                message["files"].append(file)
+                
+        if config.stats_use_embed and config.thumbnail_in_stats:
+            thumbnail = renderer.get_thumbnail(address=(ip, int(port)))
+            with BytesIO() as thumbnail_output:
+                thumbnail.save(thumbnail_output, format="PNG")
+                thumbnail_output.seek(0)
+                file = discord.File(fp=thumbnail_output, filename="thumbnail.png")
+                message["embed"].set_thumbnail(url="attachment://thumbnail.png")
+                message["files"].append(file)
+            
+        await ctx.send(**message)
+
+    #команда для выполнения команды в mc
+    @commands.command(name="run")
+    async def run_cmd(self, ctx):
+        if config.console_channel == False:
+            return
+        if not ctx.channel.id == config.console_channel_id:
+            return
+        
+        cmd = ctx.message.content.replace(ctx.prefix + "run", "", 1).lstrip()
+        try:
+            response = self.client.rcon.send_cmd(cmd)
+        except (BrokenPipeError, ConnectionRefusedError, struct.error) as e:
+            print("Попытка повторного подключения к серверу RCON...")
+            try:
+                self.client.rcon.reconnect()
+                response = self.client.rcon.send_cmd(cmd)
+                print("Повторное подключение успешно!")
+            except ConnectionRefusedError:
+                response = "Ошибка: Подключение к серверу RCON было отклонено. Сервер выключен/запускается или RCON не включен."
+
+        if response == "":
+            response = "Команда была успешно выполнена, но ответ от сервера был пустым."
+        max_length = 2000-len(config.run_output.lstrip().strip())
+        msg = config.run_output.format(output=textwrap.shorten(response, width=max_length)) 
+        
+        await ctx.send(msg)
+
+    #команда для отображения серверного motd
+    @commands.command(name="motd")
+    async def get_motd(self, ctx, address="127.0.0.1", port=None):
+        if port == None:
+            port = self.client.server_address[1]
+        if address.startswith("192.") or address=="localhost" or address=="0.0.0.0":
+            await ctx.send(config.motd_invalid_ip_message)
+            return
+        if address == "127.0.0.1" and not port==self.client.server_address[1]:
+            await ctx.send(config.motd_invalid_ip_message)
+            return
+        if address == "127.0.0.1":
+            title = config.motd_title
+        else:
+            title = address+":"+str(port)
+
+        message_old = await ctx.send(config.motd_pinging_message)
+
+        message = {}
+        if config.motd_use_embed:
+            message["embed"] = embed_builder(config.motd_embed, {})
+        else:
+            message["content"] = config.motd_message.strip().lstrip()
+
+        renderer = motd_renderer.MOTDRenderer()
+        image = renderer.get_full_image(title=title, address=(address, port))
+        with BytesIO() as output:
+            image.save(output, format="PNG")
+            output.seek(0)
+            message["file"] = discord.File(fp=output, filename="motd.png")
+            if config.motd_use_embed:
+                message["embed"].set_image(url="attachment://motd.png")
+
+        await ctx.send(**message)
+        await message_old.delete()
+
+    #команда помощи
+    @commands.command(name="help")
+    async def help(self, ctx):
+        message = {}
+        if config.help_use_embed:
+            message["embed"] = embed_builder(config.help_output_embed, {})
+        else:
+            await ctx.send(config.help_message.lstrip().strip())
+            return
+        
+        if config.thumbnail_in_help:
+            renderer = motd_renderer.MOTDRenderer()
+            ip, port = config.server_address.split(":")
+            message["files"] = []
+            thumbnail = renderer.get_thumbnail(address=(ip, int(port)))
+            with BytesIO() as thumbnail_output:
+                thumbnail.save(thumbnail_output, format="PNG")
+                thumbnail_output.seek(0)
+                file = discord.File(fp=thumbnail_output, filename="thumbnail.png")
+                message["embed"].set_thumbnail(url="attachment://thumbnail.png")
+                message["files"].append(file)
+        await ctx.send(**message)
